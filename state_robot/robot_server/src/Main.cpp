98b9dc0ab90f8b0491d72aa751386648415a9954
tommydang1 <52802269+tommydang1@users.noreply.github.com>
2023-05-23 13:32:11 -0700

    Add files via upload

---
+++ state_robot/robot_server/src/Main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+	// use these strings to indicate the state transitions
+	// the robot progresses through.  Do not modify these strings
+
+	std::string robot_waiting = "The robot is waiting";
+	std::string robot_moving = "The robot is moving";
+
+	std::string robot_finished_waiting = "The robot finished waiting";
+	std::string robot_finished_moving = "The robot finished moving";
+
+	std::string robot_began_waiting = "The robot began waiting";
+	std::string robot_begin_moving = "The robot begain moving";
+
+	return EXIT_SUCCESS;
+}
+
